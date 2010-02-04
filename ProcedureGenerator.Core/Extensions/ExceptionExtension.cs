e2e37d95d5aaa64bc0eb8b7a3932469f53ef8242
MrKevHunter <MrKevHunter@ac2f74de-1035-11df-b192-4df06d6974c1>
2010-02-04 15:43:59 +0000

    renamed worked completed method

---
+++ ProcedureGenerator.Core/Extensions/ExceptionExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProcedureGenerator.Core.Extensions
+{
+	public static class ExceptionExtension
+	{
+		public static Exception GetInnermostException(this Exception e)
+		{
+			while (e.InnerException != null)
+			{
+				e = e.InnerException;
+			}
+			return e;
+		}
+	}
+}
