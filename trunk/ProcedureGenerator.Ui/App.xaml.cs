1e5a785c6ad056539945ca716af26fa6e67810c5
MrKevHunter <MrKevHunter@ac2f74de-1035-11df-b192-4df06d6974c1>
2010-02-04 23:44:02 +0000

    removed all the code behind, everything is done in the view model going to use binding for everything we can

--- trunk/ProcedureGenerator.Ui/App.xaml.cs
+++ trunk/ProcedureGenerator.Ui/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using ProcedureGenerator.Ui.View;
+using ProcedureGenerator.Ui.ViewModel;
 
 namespace ProcedureGenerator.Ui
 {
@@ -12,5 +14,23 @@ namespace ProcedureGenerator.Ui
    /// </summary>
    public partial class App : Application
    {
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			MainView window = new MainView();
+
+			var viewModel = new MainViewModel();
+         
+			// Allow all controls in the window to 
+			// bind to the ViewModel by setting the 
+			// DataContext, which propagates down 
+			// the element tree.
+			window.DataContext = viewModel;
+			window.btnLoadTables.Click += viewModel.LoadTables;
+			window.btnGenerate.Click += viewModel.GenerateStoredProcedures;
+			window.Show();
+		}
    }
 }
