2c4e43870a74bb7b042e4dac70c408a46bbcbd31
MrKevHunter <MrKevHunter@ac2f74de-1035-11df-b192-4df06d6974c1>
2010-02-04 20:07:53 +0000

    Moving to MV VM

---
+++ ProcedureGenerator.Ui/Convertors/BoolToVisibleConvertor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ProcedureGenerator.Ui
+{
+	public class BoolToVisibleConvertor : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool visible = (bool) value;
+			return visible ? Visibility.Visible : Visibility.Hidden;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Visibility.Hidden;
+		}
+	}
+}
---
+++ ProcedureGenerator.Ui/ViewModel/MainViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.ObjectModel;
+
+namespace ProcedureGenerator.Ui.ViewModel
+{
+	public class MainViewModel
+	{
+		public ObservableCollection<TableDto> Tables { get; private set;}
+	}
+}
