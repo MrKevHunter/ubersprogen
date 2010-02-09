5d4a69d03968649b1faa4f9f79c4542e9431a093
MrKevHunter <MrKevHunter@ac2f74de-1035-11df-b192-4df06d6974c1>
2010-02-09 20:19:45 +0000

    fixed bug where identity columns where not being detected

--- ProcedureGenerator.Ui/Services/DbTableService.cs
+++ ProcedureGenerator.Ui/Services/DbTableService.cs
@@ -1,7 +1,31 @@
+using System.Collections.ObjectModel;
+using System.Linq;
+using ProcedureGenerator.Core.DataAccess;
+using ProcedureGenerator.Ui.Dto;
+
 namespace ProcedureGenerator.Ui.Services
 {
 	public class DbTableService
 	{
-		 
+		 public ObservableCollection<TableDto> GetTables(string connectionString)
+		 {
+			 var schemaDataContext = new SchemaDataContext(connectionString);
+			 var tables = new ObservableCollection<TableDto>();
+			 foreach (
+				 TABLE table in
+					 schemaDataContext.TABLEs.Where(table => table.TABLE_TYPE == "BASE TABLE").OrderBy(
+						 table1 => table1.TABLE_NAME))
+			 {
+
+				 tables.Add(new TableDto
+				 {
+					 IsChecked = false,
+					 TableName = table.TABLE_NAME,
+					 HasPrimaryKey = false,
+					 HasForeignKey = true
+				 });
+			 }
+		 	return tables;
+		 }
 	}
 }
