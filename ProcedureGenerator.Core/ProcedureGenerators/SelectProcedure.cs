935ae8a909f5363f184a2b0380706be03ef29b29
bhofmann.hotmail.co.uk <bhofmann.hotmail.co.uk@ac2f74de-1035-11df-b192-4df06d6974c1>
2011-01-06 15:47:23 +0000

    Prettier velocity templates.

--- ProcedureGenerator.Core/ProcedureGenerators/SelectProcedure.cs
+++ ProcedureGenerator.Core/ProcedureGenerators/SelectProcedure.cs
@@ -1,13 +1,21 @@
 namespace ProcedureGenerator.Core.ProcedureGenerators
 {
-   public class SelectProcedure : SelectByIdProcedure
-   {
-      protected override string ProcedureTemplatePath
-      {
-         get
-         {
-            return "/Velocity/Select/SelectAll.Vm";
-         }
-      }
-   }
+	public class SelectProcedure : SelectByIdProcedure
+	{
+		protected override string ProcedureName
+		{
+			get
+			{
+				return string.Format("sp{0}SelectAll", Table.Name);
+			}
+		}
+
+		protected override string ProcedureTemplatePath
+		{
+			get
+			{
+				return "/Velocity/Select/SelectAll.Vm";
+			}
+		}
+	}
 }
