b59d5f265a92567275b8e3ec5bd35f1d028dfd60
bhofmann.hotmail.co.uk <bhofmann.hotmail.co.uk@ac2f74de-1035-11df-b192-4df06d6974c1>
2011-01-06 17:19:30 +0000

    Implemented new feature for issue 14 - Allow user-specified schema for sprocs

--- trunk/ProcedureGenerator.Core/ProcedureGenerators/ForeignKeyProcedure.cs
+++ trunk/ProcedureGenerator.Core/ProcedureGenerators/ForeignKeyProcedure.cs
@@ -1,52 +1,48 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
 using ProcedureGenerator.Core.Domain;
 using ProcedureGenerator.Core.Velocity;
 
 namespace ProcedureGenerator.Core.ProcedureGenerators
 {
-   public class ForeignKeyProcedure : GenerateProcedureBase , IGenerateMultipleProcedure
-   {
-      private Column _foreignKey;
-      protected override string ProcedureName
-      {
-         get
-         {
-            return string.Format("sp{0}SelectBy{1}", Table.Name, _foreignKey);
-         }
-      }
+	public class ForeignKeyProcedure : GenerateProcedureBase, IGenerateMultipleProcedure
+	{
+		private Column _foreignKey;
+		protected override string ProcedureName
+		{
+			get
+			{
+				return string.Format("sp{0}SelectBy{1}", Table.Name, _foreignKey);
+			}
+		}
 
-      protected override string ProcedureTemplatePath
-      {
-         get
-         {
-            return "/velocity/Select/SelectByFk.vm";
-         }
-      }
+		protected override string ProcedureTemplatePath
+		{
+			get
+			{
+				return "/velocity/Select/SelectByFk.vm";
+			}
+		}
 
-   	private ProcedureConfiguration procConfig;
+		private ProcedureConfiguration procConfig;
 
-      public IEnumerable<Procedure> GenerateProcedures(Table t, ProcedureConfiguration procConfig)
-      {
-      	this.procConfig = procConfig;
-         Table = t;
-         foreach (Column foreignKey in t.ForeignKeys)
-         {
-            _foreignKey = foreignKey;
+		public IEnumerable<Procedure> GenerateProcedures(Table t, ProcedureConfiguration procConfig)
+		{
+			this.procConfig = procConfig;
+			Table = t;
+			foreach (var foreignKey in t.ForeignKeys)
+			{
+				_foreignKey = foreignKey;
 				yield return Generate(t, procConfig);
+			}
+		}
 
-         }
-
-      }
-
-      protected override string GenerateBody()
-      {
-         var procedureGeneration = new VelocityProcedureGeneration(ProcedureTemplatePath,
-                                                          string.Format("sp{0}SelectBy{1}", Table.Name,
-																								_foreignKey), Owner, Table, procConfig);
-         procedureGeneration.AddKeys(new KeyValuePair<string, object>("key", _foreignKey));
-         return procedureGeneration.Execute();
-      }
-   }
+		protected override string GenerateBody()
+		{
+			var procedureGeneration = new VelocityProcedureGeneration(ProcedureTemplatePath,
+																			 string.Format("sp{0}SelectBy{1}", Table.Name,
+																								_foreignKey), Table, procConfig);
+			procedureGeneration.AddKeys(new KeyValuePair<string, object>("key", _foreignKey));
+			return procedureGeneration.Execute();
+		}
+	}
 }
--- trunk/ProcedureGenerator.Core/ProcedureGenerators/GenerateProcedureBase.cs
+++ trunk/ProcedureGenerator.Core/ProcedureGenerators/GenerateProcedureBase.cs
@@ -5,54 +5,44 @@ using ProcedureGenerator.Core.Velocity;
 
 namespace ProcedureGenerator.Core.ProcedureGenerators
 {
-   public abstract class GenerateProcedureBase :IGenerateProcedure
-   {
+	public abstract class GenerateProcedureBase : IGenerateProcedure
+	{
 
-      protected abstract string ProcedureName { get; }
+		protected abstract string ProcedureName { get; }
 
-		protected ProcedureConfiguration ProcConfig { get; set;}
+		protected ProcedureConfiguration ProcConfig { get; set; }
 
-      protected Table Table { get; set;}
-      public virtual Procedure Generate(Table t, ProcedureConfiguration procedureConfig)
-      {
-      	ProcConfig = procedureConfig;
-         Table = t;
-         var sb = new StringBuilder();
-         sb.AppendLine(GenerateExistenceCheck());
-         sb.AppendLine(GenerateComments());
-         sb.AppendLine(GenerateBody());
-         return new Procedure {Body = sb.ToString(),Name = ProcedureName};
-      }
+		protected Table Table { get; set; }
+		public virtual Procedure Generate(Table t, ProcedureConfiguration procedureConfig)
+		{
+			ProcConfig = procedureConfig;
+			Table = t;
+			var sb = new StringBuilder();
+			sb.AppendLine(GenerateExistenceCheck());
+			sb.AppendLine(GenerateComments());
+			sb.AppendLine(GenerateBody());
+			return new Procedure { Body = sb.ToString(), Name = ProcedureName };
+		}
 
-      protected virtual string GenerateBody()
-      {
-			var procedureGeneration = new VelocityProcedureGeneration(ProcedureTemplatePath, ProcedureName, Owner, Table, ProcConfig);
-         return procedureGeneration.Execute();
-      }
+		protected virtual string GenerateBody()
+		{
+			var procedureGeneration = new VelocityProcedureGeneration(ProcedureTemplatePath, ProcedureName, Table, ProcConfig);
+			return procedureGeneration.Execute();
+		}
 
-      protected virtual string GenerateComments()
-      {
-         string path = "/velocity/common/comment.vm";
-         return new VelocityProcedureGeneration(path, ProcedureName).Execute();
-      }
+		protected virtual string GenerateComments()
+		{
+			const string path = "/velocity/common/comment.vm";
+			return new VelocityProcedureGeneration(path, ProcedureName).Execute();
+		}
 
+		protected abstract string ProcedureTemplatePath { get; }
 
-      protected virtual string Owner
-      {
-         get
-         {
-            return "dbo";
-         }
-
-      }
-
-      protected abstract string ProcedureTemplatePath { get; }
-
-      protected virtual string GenerateExistenceCheck()
-      {
-         var existenceCheck = "/velocity/common/ProcedureDropExisting.vm";
-         var procedureGeneration = new VelocityProcedureGeneration(existenceCheck, ProcedureName,"dbo",null, new ProcedureConfiguration());
-         return procedureGeneration.Execute();
-      }
-   }
+		protected virtual string GenerateExistenceCheck()
+		{
+			const string existenceCheck = "/velocity/common/ProcedureDropExisting.vm";
+			var procedureGeneration = new VelocityProcedureGeneration(existenceCheck, ProcedureName, Table, ProcConfig);
+			return procedureGeneration.Execute();
+		}
+	}
 }
